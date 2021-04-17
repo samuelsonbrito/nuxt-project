2d9c4d914c00855321dd90899fb05d913e0a6139
Samuelson Brito <samuelsonma@gmail.com>
2021-04-17 14:12:21 -0400

    crja

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Página não encontrada</h1>
+    <h1 v-else>Ocorreu um erro</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['error'],
+    layout: 'error' // você pode definir um layout personalizado para a página de erro
+}
+</script>
+
+<style>
+
+</style>
